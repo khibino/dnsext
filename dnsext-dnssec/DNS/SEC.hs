279b80b2b5f2351b494b7abd11c5797f8d70d718
Kei Hibino <ex8k.hibino@gmail.com>
2022-10-18 00:53:47 +0900

    export verify-rrsig

--- dnsext-dnssec/DNS/SEC.hs
+++ dnsext-dnssec/DNS/SEC.hs
@@ -1,7 +1,9 @@
 module DNS.SEC (
     module DNS.SEC.Types
   , module DNS.SEC.Time
+  , module DNS.SEC.Verify.RRSIG
   ) where
 
 import DNS.SEC.Time
 import DNS.SEC.Types
+import DNS.SEC.Verify.RRSIG
