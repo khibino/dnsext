dc9494d5b8125261cea12213401dcaa1f96fbb78
Kazu Yamamoto <kazu@iij.ad.jp>
2023-04-25 11:55:09 +0900

    enriching error messages

--- dnsext-types/DNS/Types/Domain.hs
+++ dnsext-types/DNS/Types/Domain.hs
@@ -391,15 +391,11 @@ getDomain' allowCompression ptrLimit = do
       | isPointer c = do
           d <- getInt8
           let offset = n * 256 + d
-          when (offset == ptrLimit) $
-              failSGet "invalid pointer: self pointing"
-          when (offset > ptrLimit) $
-              failSGet "invalid pointer: forward pointing"
+          when (offset == ptrLimit) $ failure "self pointing" pos offset
+          when (offset > ptrLimit)  $ failure "forward pointing" pos offset
           mx <- popDomain offset
           case mx of
-            Nothing -> do
-              domains <- getPstDomains
-              failSGet $ "invalid pointer: invalid area: offset=" ++ show offset ++ ", "++ show (IM.toList domains)
+            Nothing -> failure "invalid area" pos offset
             Just lls -> do
                 -- Supporting double pointers.
                 pushDomain pos lls
@@ -415,6 +411,7 @@ getDomain' allowCompression ptrLimit = do
     getValue c = c .&. 0x3f
     isPointer c = testBit c 7 && testBit c 6
     isExtLabel c = not (testBit c 7) && testBit c 6
+    failure msg pos offset = failSGet $ "invalid pointer " ++ show offset ++ " at " ++ show pos ++ ": " ++ msg
 
 ----------------------------------------------------------------
 
