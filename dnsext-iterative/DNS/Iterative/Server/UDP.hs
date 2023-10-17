da5cf584b2acdd479953b7a38f2f4a51d772e97e
Kei Hibino <ex8k.hibino@gmail.com>
2023-10-17 14:31:28 +0900

    worker-stats: add worker-thread stat to UDP-pipeline

--- dnsext-iterative/DNS/Iterative/Server/UDP.hs
+++ dnsext-iterative/DNS/Iterative/Server/UDP.hs
@@ -4,7 +4,7 @@
 module DNS.Iterative.Server.UDP where
 
 -- GHC packages
-import Control.Monad (forever, replicateM)
+import Control.Monad (forever, replicateM, when)
 import Data.ByteString (ByteString)
 import Control.Concurrent.STM (atomically, newTQueueIO, readTQueue, writeTQueue)
 
@@ -32,6 +32,7 @@ import DNS.Iterative.Queue (
 import qualified DNS.Iterative.Queue as Queue
 import DNS.Iterative.Server.Pipeline
 import DNS.Iterative.Server.Types
+import DNS.Iterative.Server.WorkerStats
 import DNS.TAP.Schema (SocketProtocol (..))
 
 ----------------------------------------------------------------
@@ -49,6 +50,8 @@ type Response a = (ByteString, a)
 
 type EnqueueDec a = Decoded a -> IO ()
 type EnqueueResp a = Response a -> IO ()
+
+type UdpWorkerStats = [[WorkerStatOP]]
 
 ----------------------------------------------------------------
 
@@ -70,13 +73,13 @@ type EnqueueResp a = Response a -> IO ()
 
 ----------------------------------------------------------------
 
-udpServer :: UdpServerConfig -> Server
-udpServer conf env port addr = do
+udpServer :: UdpWorkerStats -> UdpServerConfig -> Server
+udpServer scstats conf env port addr = do
     lsock <- UDP.serverSocket (read addr, port)
     let mysa = UDP.mySockAddr lsock
         putLn lv = logLines_ env lv Nothing . (: [])
 
-    (mkPipelines, enqueueReq, dequeueResp) <- getPipelines conf env mysa
+    (mkPipelines, enqueueReq, dequeueResp) <- getPipelines scstats conf env mysa
     pipelines <- sequence mkPipelines
 
     let onErrorR = putLn Log.WARN . ("Server.recvRequest: error: " ++) . show
@@ -88,25 +91,35 @@ udpServer conf env port addr = do
 
 ----------------------------------------------------------------
 
+getUdpWorkerStats :: UdpServerConfig -> IO UdpWorkerStats
+getUdpWorkerStats UdpServerConfig{..} =
+    replicateM udp_pipelines_per_socket $ replicateM udp_workers_per_pipeline getWorkerStatOP
+
+----------------------------------------------------------------
+
 getPipelines
-    :: UdpServerConfig
+    :: UdpWorkerStats
+    -> UdpServerConfig
     -> Env
     -> SockAddr
     -> IO ([IO [IO ()]], Request UDP.ClientSockAddr -> IO (), IO (Response UDP.ClientSockAddr))
-getPipelines udpconf@UdpServerConfig{..} env mysa
+getPipelines scstats udpconf@UdpServerConfig{..} env mysa
+    | length scstats /= udp_pipelines_per_socket = do
+        let neq = "len:" ++ show (length scstats) ++ " =/= " ++ "conf:" ++ show udp_pipelines_per_socket
+        fail $ "UDP.getPipelines: internal error, inconsistent WorkerStatOP Set: " ++ neq
     | udp_queue_size_per_pipeline <= 0 = do
         reqQ <- newQueueChan
         resQ <- newQueueChan
         {- share request queue and response queue -}
         let udpconf' = udpconf{udp_queue_size_per_pipeline = 8}
-            wps = replicate udp_pipelines_per_socket $ getCacherWorkers reqQ resQ udpconf' env mysa
+            wps = [getCacherWorkers reqQ resQ sts udpconf' env mysa | sts <- scstats]
         return (wps, writeQueue reqQ, readQueue resQ)
     | udp_pipeline_share_queue = do
         -- let qsize = udp_queue_size_per_pipeline * udp_pipelines_per_socket
         reqQ <- newTQueueIO
         resQ <- newTQueueIO
         {- share request queue and response queue -}
-        let wps = replicate udp_pipelines_per_socket $ getCacherWorkers reqQ resQ udpconf env mysa
+        let wps = [getCacherWorkers reqQ resQ sts udpconf env mysa | sts <- scstats]
         return (wps, atomically . writeTQueue reqQ, atomically $ readTQueue resQ)
     | otherwise = do
         reqQs <- replicateM udp_pipelines_per_socket $ newQueue udp_queue_size_per_pipeline
@@ -114,9 +127,10 @@ getPipelines udpconf@UdpServerConfig{..} env mysa
         resQs <- replicateM udp_pipelines_per_socket $ newQueue udp_queue_size_per_pipeline
         dequeueResp <- Queue.readQueue <$> Queue.makeGetAny resQs
         let wps =
-                [ getCacherWorkers reqQ resQ udpconf env mysa
+                [ getCacherWorkers reqQ resQ sts udpconf env mysa
                 | reqQ <- reqQs
                 | resQ <- resQs
+                | sts <- scstats
                 ]
         return (wps, enqueueReq, dequeueResp)
 
@@ -126,17 +140,24 @@ getCacherWorkers
     :: (ReadQueue rq, QueueSize rq, WriteQueue wq, QueueSize wq)
     => rq (Request UDP.ClientSockAddr)
     -> wq (Response UDP.ClientSockAddr)
+    -> [WorkerStatOP]
     -> UdpServerConfig
     -> Env
     -> SockAddr
     -> IO ([IO ()])
-getCacherWorkers reqQ resQ UdpServerConfig{..} env mysa = do
+getCacherWorkers reqQ resQ wsts UdpServerConfig{..} env mysa = do
     let logr = putLn Log.WARN . ("Server.worker: error: " ++) . show
     (resolvLoop, enqueueDec, _decQSize) <- do
         inQ <- newTQueueIO
-        let loop = handledLoop logr $ do
+        let loop WorkerStatOP{..} = handledLoop logr $ do
+                setWorkerStat WWaitDequeue
                 (reqMsg, clisa@(UDP.ClientSockAddr peersa _)) <- atomically $ readTQueue inQ
-                let enqueueResp' x = enqueueResp (x, clisa)
+                case DNS.question reqMsg of
+                    q : _ -> setWorkerStat (WRun q)
+                    [] -> pure ()
+                let enqueueResp' x = do
+                        setWorkerStat WWaitEnqueue
+                        enqueueResp (x, clisa)
                 workerLogic env enqueueResp' UDP mysa peersa reqMsg
         return (loop, atomically . writeTQueue inQ, pure (-1) :: IO Int)
 
@@ -147,7 +168,11 @@ getCacherWorkers reqQ resQ UdpServerConfig{..} env mysa = do
                 enqueueResp' x = enqueueResp (x, clisa)
             cacherLogic env enqueueResp' DNS.decodeAt enqueueDec' UDP mysa peersa req
 
-        resolvLoops = replicate udp_workers_per_pipeline resolvLoop
+    when (length wsts /= udp_workers_per_pipeline) $ do
+        let neq = "len:" ++ show (length wsts) ++ " =/= " ++ "conf:" ++ show udp_workers_per_pipeline
+        fail $ "UDP.getCacherWorkers: internal error, inconsistent length of WorkerStatOP list: " ++ neq
+
+    let resolvLoops = map resolvLoop wsts
         loops = resolvLoops ++ [cachedLoop]
 
     return loops
