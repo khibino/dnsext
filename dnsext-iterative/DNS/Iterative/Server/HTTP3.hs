2bed7f77230787742553bd3d88ff0f3fd1306f46
Kei Hibino <ex8k.hibino@gmail.com>
2024-06-28 03:01:32 +0900

    DoH3, h3 receiver-loop, h3 sender-loop

--- dnsext-iterative/DNS/Iterative/Server/HTTP3.hs
+++ dnsext-iterative/DNS/Iterative/Server/HTTP3.hs
@@ -6,13 +6,16 @@ module DNS.Iterative.Server.HTTP3 (
 ) where
 
 -- GHC packages
-
+import Control.Concurrent.STM (atomically)
+import qualified Control.Exception as E
+import Control.Monad (when)
 import Data.ByteString.Builder (byteString)
 import qualified Data.ByteString.Char8 as C8
 
 -- dnsext-* packages
 import qualified DNS.Log as Log
 import DNS.TAP.Schema (SocketProtocol (..))
+import qualified DNS.ThreadStats as TStat
 
 -- other packages
 
@@ -55,18 +58,38 @@ doHTTP env toCacher req aux sendResponse = do
     let mysa = H2.auxMySockAddr aux
         peersa = H2.auxPeerSockAddr aux
         peerInfo = PeerInfoVC peersa
-    (toSender, fromX, _) <- mkConnector
-    einp <- getInput req
-    case einp of
-        Left emsg -> logLn env Log.WARN $ "decode-error: " ++ emsg
-        Right bs -> do
-            let inp = Input bs 0 mysa peerInfo DOH toSender
-            incStatsDoH3 peersa (stats_ env)
-            toCacher inp
-            Output bs' _ _ <- fromX
-            let header = mkHeader bs'
-                response = H2.responseBuilder HT.ok200 header $ byteString bs'
-            sendResponse response []
+    logLn env Log.DEBUG $ "h3-srv: accept: " ++ show peersa
+    (toSender, fromX, availX) <- mkConnector
+    (eof, pendings) <- mkVcState
+    let receiver = loop 1 *> atomically (enableVcEof eof)
+          where
+            loop i = do
+                einp <- getInput' req
+                case einp of
+                    Left emsg -> logLn env Log.WARN $ "h3-srv: decode-error: " ++ emsg
+                    Right ("", True)  -> pure ()
+                    Right (bs, _) -> step i bs *> loop (succ i)
+            step i bs = do
+                atomically (addVcPending pendings i)
+                let inp = Input bs i mysa peerInfo DOH toSender
+                incStatsDoH3 peersa (stats_ env)
+                toCacher inp
+
+        sender = loop `E.catch` onError
+          where
+            onError (E.SomeException e) = logLn env Log.WARN ("h3-srv: exception: " ++ show e) *> E.throwIO e
+            loop = do
+                avail <- atomically (waitVcAvail eof pendings availX)
+                when avail $ step *> loop
+            step = do
+                let body (Output bs' _ _) = do
+                        let header = mkHeader bs'
+                            response = H2.responseBuilder HT.ok200 header $ byteString bs'
+                        sendResponse response []
+                    finalize (Output _ i _) = atomically (delVcPending pendings i)
+                E.bracket fromX finalize body
+    TStat.concurrently_ "h3-send" sender "h3-recv" receiver
+    logLn env Log.DEBUG $ "h3-srv: close: " ++ show peersa
   where
     -- fixme record
     mkHeader bs =
