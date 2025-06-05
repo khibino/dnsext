42d23a17f880d23caa92523a26f9e9eb607396d0
Kei Hibino <ex8k.hibino@gmail.com>
2025-06-05 19:03:56 +0900

    dnsext-svcb: update SvcParamKey defs

--- dnsext-svcb/DNS/SVCB/Key.hs
+++ dnsext-svcb/DNS/SVCB/Key.hs
@@ -11,19 +11,24 @@ newtype SvcParamKey = SvcParamKey
 
 {- FOURMOLU_DISABLE -}
 instance Show SvcParamKey where
-    show SPK_Mandatory     = "mandatory"
-    show SPK_ALPN          = "alpn"
-    show SPK_NoDefaultALPN = "no-default-alpn"
-    show SPK_Port          = "port"
-    show SPK_IPv4Hint      = "ipv4hint"
-    show SPK_ECH           = "ech"
-    show SPK_IPv6Hint      = "ipv6hint"
-    show SPK_DoHPath       = "dohpath"
-    show (SvcParamKey n)   = "SvcParamKey" ++ show n -- no space
+    show SPK_Mandatory          = "mandatory"
+    show SPK_ALPN               = "alpn"
+    show SPK_NoDefaultALPN      = "no-default-alpn"
+    show SPK_Port               = "port"
+    show SPK_IPv4Hint           = "ipv4hint"
+    show SPK_ECH                = "ech"
+    show SPK_IPv6Hint           = "ipv6hint"
+    show SPK_DoHPath            = "dohpath"
+    show SPK_OHTTP              = "ohttp"
+    show SPK_TLSSupporedGroups  = "tls-supported-groups"
+    show (SvcParamKey n)        = "SvcParamKey" ++ show n -- no space
 {- FOURMOLU_ENABLE -}
 
 toSvcParamKey :: Word16 -> SvcParamKey
 toSvcParamKey = SvcParamKey
+
+-- SvcParamKey data table:
+-- https://www.iana.org/assignments/dns-svcb/dns-svcb.xhtml
 
 pattern SPK_Mandatory :: SvcParamKey
 pattern SPK_Mandatory = SvcParamKey 0
@@ -48,3 +53,9 @@ pattern SPK_IPv6Hint = SvcParamKey 6
 
 pattern SPK_DoHPath :: SvcParamKey
 pattern SPK_DoHPath = SvcParamKey 7
+
+pattern SPK_OHTTP :: SvcParamKey
+pattern SPK_OHTTP = SvcParamKey 8
+
+pattern SPK_TLSSupporedGroups :: SvcParamKey
+pattern SPK_TLSSupporedGroups = SvcParamKey 9
