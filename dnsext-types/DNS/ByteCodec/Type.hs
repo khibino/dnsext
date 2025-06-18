257c0932d27ff4a8377e393fac9f47ea65c1e19b
Kei Hibino <ex8k.hibino@gmail.com>
2025-06-18 10:27:18 +0900

    temp, dnsext-types: interface for zero-copy byte-squence writer

--- dnsext-types/DNS/ByteCodec/Type.hs
+++ dnsext-types/DNS/ByteCodec/Type.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE MagicHash #-}
+{-# LANGUAGE UnboxedTuples #-}
 
 module DNS.ByteCodec.Type where
 
 -- GHC exts
 import Control.Monad.ST (RealWorld)
 import Control.Monad
-import Data.Char (ord)
+import Data.Char (chr, ord)
 import Data.IORef (IORef, modifyIORef, newIORef, readIORef, writeIORef)
 import Foreign (Ptr, Storable (..))
 import GHC.Exts (ByteArray#, MutableByteArray#, (+#), (-#))
@@ -43,7 +44,13 @@ class ByteSeqRead b where
 {- FOUMOLU_ENABLE -}
 
 class ByteSeqRead b => SeqReader s b | s -> b where
-    unsafeBufferedRead :: s -> (b -> IO a) -> IO a
+    unsafeBufferRead :: s -> (b -> IO a) -> IO a
+
+class ByteSeqWrite b where
+    unsafeWrite1  :: b -> Word8 -> IO ()
+
+class ByteSeqWrite b => SeqWriter s b | s -> b where
+    unsafeCreateBytes :: Int -> (b -> IO ()) -> IO s
 
 ---
 
@@ -61,7 +68,7 @@ instance ByteSeqRead BufferSBS where
 
 {- FOUMOLU_DISABLE -}
 instance SeqReader ShortByteString BufferSBS where
-    unsafeBufferedRead s k = do
+    unsafeBufferRead s k = do
         off   <- newIORef 0
         rest  <- newIORef (SBS.length s)
         k (s, off, rest)
@@ -81,7 +88,7 @@ instance ByteSeqRead BufferBS where
 
 {- FOUMOLU_DISABLE -}
 instance SeqReader ByteString BufferBS where
-    unsafeBufferedRead s k = do
+    unsafeBufferRead s k = do
         off   <- newIORef 0
         rest  <- newIORef (BS.length s)
         k (s, off, rest)
@@ -104,7 +111,7 @@ instance ByteSeqRead (IORef String, IORef Int) where
 
 {- FOUMOLU_DISABLE -}
 instance SeqReader String BufferStr where
-    unsafeBufferedRead s k = do
+    unsafeBufferRead s k = do
         sref  <- newIORef s
         rest  <- newIORef (length s)
         k (sref, rest)
@@ -119,7 +126,7 @@ instance SeqReader String BufferStr where
 [98,97,122]
  -}
 readBytes :: SeqReader s b => s -> IO [Word8]
-readBytes s = unsafeBufferedRead s loop
+readBytes s = unsafeBufferRead s loop
   where
     loop b = do
         rlen <- restSize b
@@ -127,3 +134,64 @@ readBytes s = unsafeBufferedRead s loop
                 | rlen > 0   = (:) <$> unsafeRead1 b <*> loop b
                 | otherwise  = pure []
         loop'
+
+---
+
+data MBA = MBA# (MutableByteArray# RealWorld)
+type BufferMBA = (MBA, IORef Int)
+
+instance ByteSeqWrite BufferMBA where
+    unsafeWrite1 (MBA# mba#, iref) (W8# w#) = do
+        I# i# <- readIORef iref
+        writeIORef iref $ I# (i# +# 1#)
+        IO $ \s -> case GHC.writeWord8Array# mba# i# w# s of s1 -> (# s1, () #)
+
+instance SeqWriter ShortByteString BufferMBA where
+    unsafeCreateBytes len k = do
+        mba <- new len
+        iref <- newIORef 0
+        k (mba, iref)
+        freeze mba
+      where
+        new (I# len#) = IO $ \s -> case GHC.newByteArray# len# s of (# s1, mba# #) -> (# s1, MBA# mba# #)
+        freeze (MBA# mba#) = IO $ \s -> case GHC.unsafeFreezeByteArray# mba# s of (# s1, ba# #) -> (# s1, SBS ba# #)
+
+type BufferPtr = (Ptr Word8, IORef Int)
+
+instance ByteSeqWrite BufferPtr where
+    unsafeWrite1 (ptr, iref) w8 = do
+        i <- readIORef iref
+        writeIORef iref (i + 1)
+        pokeByteOff ptr i w8
+
+instance SeqWriter ByteString BufferPtr where
+    unsafeCreateBytes len k = BS.create len (\ptr -> newIORef 0 >>= \ iref -> k (ptr, iref))
+
+type BufferShowS = IORef ShowS
+
+instance ByteSeqWrite BufferShowS where
+    unsafeWrite1 sref w8 = modifyIORef sref (\s -> s . (chr (fromIntegral w8) :))
+
+instance SeqWriter String BufferShowS where
+    unsafeCreateBytes len k = do
+        sref <- newIORef id
+        k sref
+        ($ []) <$> readIORef sref
+
+{- FOUMOLU_DISABLE -}
+{- |
+>>> copyBytes @ShortByteString @ByteString "foo"
+"foo"
+>>> copyBytes @ByteString @String "bar"
+"bar"
+>>> copyBytes @String @ShortByteString "baz"
+"baz"
+ -}
+copyBytes :: forall s1 s2 b1 b2 . (SeqReader s1 b1, SeqWriter s2 b2) => s1 -> IO s2
+copyBytes s =
+    unsafeBufferRead s $ \b1 -> do
+        len <- restSize b1
+        unsafeCreateBytes len $ \b2 -> replicateM_ len $ do
+            w8 <- unsafeRead1 b1
+            unsafeWrite1 b2 w8
+{- FOUMOLU_ENABLE -}
