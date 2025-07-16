3077bb91285bd40837d2719c22ebb9fc4b84e200
Hajime Tazaki <thehajime@gmail.com>
2025-07-16 09:55:02 +0900

    bowline: add dnstap related information to log
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- dnsext-bowline/bowline/DNSTAP.hs
+++ dnsext-bowline/bowline/DNSTAP.hs
@@ -15,13 +15,14 @@ import DNS.TAP.Schema
 import qualified DNS.ThreadStats as TStat
 import Data.IORef
 import Network.Socket
+import qualified DNS.Log as Log
 
 import Config
 
-new :: Config -> IO (IO (Maybe ThreadId), Message -> IO ())
-new conf@Config{..}
+new :: Config -> Log.PutLines IO -> IO (IO (Maybe ThreadId), Message -> IO ())
+new conf@Config{..} logP
     | cnf_dnstap = do
-        (writer, put) <- newDnstapWriter conf
+        (writer, put) <- newDnstapWriter conf logP
         return (Just <$> TStat.forkIO "dnstap-writer" writer, put)
     | otherwise = do
         let put ~_ = return ()
@@ -42,24 +43,25 @@ writeDnstapQ (DnstapQ q) ~msg = atomically $ writeTBQueue q msg
 readDnsTapQ :: DnstapQ -> IO Message
 readDnsTapQ (DnstapQ q) = atomically $ readTBQueue q
 
-newDnstapWriter :: Config -> IO (IO (), Message -> IO ())
-newDnstapWriter conf = do
+newDnstapWriter :: Config -> Log.PutLines IO -> IO (IO (), Message -> IO ())
+newDnstapWriter conf logP = do
     q <- newDnstapQ
     ref <- newIORef False
-    let logger = control conf $ exec conf q ref
+    let logger = control conf $ exec conf q ref logP
         put ~x = do
             ready <- readIORef ref
             when ready $ writeDnstapQ q x
     return (logger, put)
 
-exec :: Config -> DnstapQ -> IORef Bool -> IO ()
-exec Config{..} q ref = E.bracket setup teardown $ \sock -> do
+exec :: Config -> DnstapQ -> IORef Bool -> Log.PutLines IO -> IO ()
+exec Config{..} q ref logP = E.bracket setup teardown $ \sock -> do
     let fconf = FSTRM.Config True False
     FSTRM.writer sock fconf $ do
         msg <- readDnsTapQ q
         let d = defaultDNSTAP{dnstapMessage = Just msg}
         return $ encodeDnstap d
   where
+    logLn level = logP level Nothing . (: [])
     setup = do
         s <- E.bracketOnError open close conn
         writeIORef ref True
@@ -68,9 +70,11 @@ exec Config{..} q ref = E.bracket setup teardown $ \sock -> do
         open = socket AF_UNIX Stream defaultProtocol
         conn sock = do
             connect sock $ SockAddrUnix cnf_dnstap_socket_path
+            logLn Log.INFO ("DNSTAP connected: " ++  cnf_dnstap_socket_path)
             return sock
     teardown s = do
         writeIORef ref False
+        logLn Log.INFO ("DNSTAP disconnected: " ++  cnf_dnstap_socket_path)
         close s
 
 control :: Config -> IO () -> IO ()
