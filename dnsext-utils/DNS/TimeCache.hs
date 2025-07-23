822f6879e93fd3137c0e11d779fcadd4c2335819
Kei Hibino <ex8k.hibino@gmail.com>
2025-07-23 16:53:53 +0900

    dnsext-utils: AutoUpdate: revert. avoid linking code intended for runtimes that do not support the threaded Haskell.

--- dnsext-utils/DNS/TimeCache.hs
+++ dnsext-utils/DNS/TimeCache.hs
@@ -18,8 +18,7 @@ import Data.UnixTime (UnixTime (..), formatUnixTime, getUnixTime)
 import DNS.Types.Time (EpochTime)
 
 -- this package
-import Control.AutoUpdate
-import Control.AutoUpdate.Internal (mkClosableAutoUpdate)
+import DNS.Utils.AutoUpdate (mkClosableAutoUpdate)
 
 {- FOURMOLU_DISABLE -}
 data TimeCache = TimeCache
@@ -29,23 +28,14 @@ data TimeCache = TimeCache
     }
 {- FOURMOLU_ENABLE -}
 
+{- FOURMOLU_DISABLE -}
 newTimeCache :: IO TimeCache
 newTimeCache = do
-    let settings0 =
-            defaultUpdateSettings
-                { updateFreq = 1_000_000
-                , updateAction = getUnixTime
-                , updateThreadName = "dnsext-utils: AutoUpdate for getUnixTime"
-                }
-    (onceGetTime, close1) <- mkClosableAutoUpdate settings0
-    let settings1 =
-            defaultUpdateSettings
-                { updateFreq = 1_000_000
-                , updateAction = getTimeShowS =<< onceGetTime
-                , updateThreadName = "dnsext-utils: AutoUpdate for onceGetTime"
-                }
-    (onceGetString, close2) <- mkClosableAutoUpdate settings1
+    let interval = 1_000_000
+    (onceGetTime  , close1) <- mkClosableAutoUpdate interval  getUnixTime
+    (onceGetString, close2) <- mkClosableAutoUpdate interval (getTimeShowS =<< onceGetTime)
     return $ TimeCache (unixToEpoch <$> onceGetTime) onceGetString (close2 >> close1)
+{- FOURMOLU_ENABLE -}
 
 noneTimeCache :: TimeCache
 noneTimeCache =
