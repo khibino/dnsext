255d59e99db2f21ae014716811e4cad04fc632d3
Kei Hibino <ex8k.hibino@gmail.com>
2025-10-23 22:46:49 +0900

    iterative: norec: apply steppedWait

--- dnsext-iterative/DNS/Iterative/Query/Norec.hs
+++ dnsext-iterative/DNS/Iterative/Query/Norec.hs
@@ -26,28 +26,39 @@ import DNS.Types
 -- this package
 import DNS.Iterative.Imports
 import DNS.Iterative.Query.Class
+import DNS.Iterative.Query.SteppedWait (steppedWait)
+
+{- FOURMOLU_DISABLE -}
+norec :: MonadIO m => Env -> Bool -> NonEmpty Address -> Domain -> TYPE -> m (Either DNSError DNSMessage)
+norec cxt dnssecOK aservers name typ =
+    liftIO $ steppedWait TimeoutExpired RetryLimitExceeded 250_000 actions
+  where
+    actions = [(tag ++ ".q1", action), (tag ++ ".q2", action)]
+    tag = let (a:|as) = aservers in show (a:as)
+    action = norec_ 500_000 cxt dnssecOK aservers name typ
+{- FOURMOLU_ENABLE -}
 
 {- FOURMOLU_DISABLE -}
 {- Get the answer DNSMessage from the authoritative server.
    Note about flags in request to an authoritative server.
   * RD (Recursion Desired) must be 0 for request to authoritative server
   * EDNS must be enable for DNSSEC OK request -}
-norec :: MonadIO m => Env -> Bool -> NonEmpty Address -> Domain -> TYPE -> m (Either DNSError DNSMessage)
-norec cxt dnssecOK aservers name typ = do
+norec_ :: Int -> Env -> Bool -> NonEmpty Address -> Domain -> TYPE -> IO (Either DNSError DNSMessage)
+norec_ utimeout cxt dnssecOK aservers name typ = do
     let riActions =
             defaultResolveActions
                 { ractionGenId        = idGen_ cxt
                 , ractionGetTime      = currentSeconds_ cxt
                 , ractionLog          = logLines_ cxt
                 , ractionShortLog     = shortLog_ cxt
-                , ractionTimeoutTime  = 200_000
+                , ractionTimeoutTime  = utimeout
                 }
         ris =
             [ defaultResolveInfo
                 { rinfoIP        = aserver
                 , rinfoPort      = port
                 , rinfoActions   = riActions
-                , rinfoUDPRetry  = if length aservers == 1 then 2 else 1
+                , rinfoUDPRetry  = 1
                 , rinfoVCLimit   = 8 * 1024
                 }
             | (aserver, port) <- aservers
@@ -63,5 +74,5 @@ norec cxt dnssecOK aservers name typ = do
             | dnssecOK = FlagSet
             | otherwise = FlagClear
         qctl = DNS.rdFlag FlagClear <> DNS.doFlag doFlagSet
-    liftIO $ fmap DNS.replyDNSMessage <$> DNS.resolve renv q qctl
+    fmap DNS.replyDNSMessage <$> DNS.resolve renv q qctl
 {- FOURMOLU_ENABLE -}
