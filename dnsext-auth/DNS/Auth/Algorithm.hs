b0c19184f51d5c91d2bc301e3beccac3cf51bc2e
Kazu Yamamoto <kazu@iij.ad.jp>
2026-02-08 16:10:59 +0900

    creating dnsext-auth

---
+++ dnsext-auth/DNS/Auth/Algorithm.hs
@@ -0,0 +1,100 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module DNS.Auth.Algorithm (
+    getReply,
+) where
+
+import Data.List (nub, sort)
+import qualified Data.Map.Strict as M
+import Data.Maybe (catMaybes)
+
+import DNS.Auth.DB
+import DNS.Types
+
+-- RFC 8906: Sec 3.1.3.1
+--
+-- A non-recursive server is supposed to respond to recursive
+-- queries as if the Recursion Desired (RD) bit is not set
+getReply :: DB -> DNSMessage -> DNSMessage
+getReply db query
+    -- RFC 8906: Sec 3.1.4
+    | opcode query /= OP_STD = reply{rcode = NotImpl}
+    | not (qname q `isSubDomainOf` dbZone db) = reply{rcode = Refused}
+    | otherwise = processPositive db q reply
+  where
+    q = question query
+    -- RFC 6891: Sec 6.1.1
+    ednsH = case ednsHeader query of
+        EDNSheader _ -> EDNSheader defaultEDNS
+        _ -> NoEDNS
+    flgs =
+        DNSFlags
+            { isResponse = True
+            , authAnswer = True
+            , trunCation = False
+            , -- RFC 1035 Sec 4.1.1 -- just copy
+              recDesired = recDesired $ flags query
+            , -- RFC 1034 Sec 4.3.1
+              recAvailable = False
+            , authenData = False
+            , chkDisable = False
+            }
+    reply = query{flags = flgs, ednsHeader = ednsH}
+
+processPositive :: DB -> Question -> DNSMessage -> DNSMessage
+processPositive db@DB{..} q@Question{..} reply =
+    reply
+        { answer = ans
+        , authority = auth
+        , additional = add
+        , rcode = code
+        }
+  where
+    (ans, auth, add, code) = case M.lookup qname dbMap of
+        Nothing -> findDelegation db q
+        Just x ->
+            let ans' = case qtype of
+                    A -> rrsetA x
+                    AAAA -> rrsetAAAA x
+                    NS -> rrsetNS x
+                    _ -> filter (\r -> rrtype r == qtype) $ rrsetOthers x
+                auth' = if null ans' && qtype /= NS then rrsetNS x else []
+             in if null ans' && null auth'
+                    then
+                        ([], [dbSOA], [], NoErr)
+                    else
+                        let add' = findAdditional db auth'
+                         in (ans', auth', add', NoErr)
+
+findDelegation
+    :: DB
+    -> Question
+    -> ([ResourceRecord], [ResourceRecord], [ResourceRecord], RCODE)
+findDelegation db@DB{..} Question{..} = loop qname
+  where
+    loop dom
+        | dom == dbZone = ([], [dbSOA], [], NXDomain)
+        | otherwise = case unconsDomain dom of
+            Nothing -> ([], [dbSOA], [], NXDomain)
+            Just (_, dom') -> case M.lookup dom dbMap of
+                Nothing -> loop dom'
+                Just x ->
+                    let auth = rrsetNS x
+                     in if null auth
+                            then
+                                ([], [dbSOA], [], NoErr)
+                            else
+                                let add = findAdditional db auth
+                                 in ([], auth, add, NoErr)
+
+findAdditional :: DB -> [ResourceRecord] -> [ResourceRecord]
+findAdditional DB{..} auth' = add'
+  where
+    ns' = nub $ sort $ catMaybes $ map extractNS auth'
+    add' = concat $ map lookupAdd ns'
+    lookupAdd dom = case M.lookup dom dbGlue of
+        Nothing -> []
+        Just rs -> rs
+    extractNS rr = case fromRData $ rdata rr of
+        Nothing -> Nothing
+        Just ns -> Just $ ns_domain ns
