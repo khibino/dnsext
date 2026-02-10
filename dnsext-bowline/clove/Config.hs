a4dab3eb7664430f7d13252ff140e93bff9bf4ee
Kazu Yamamoto <kazu@iij.ad.jp>
2026-02-10 20:09:38 +0900

    using iproute

--- dnsext-bowline/clove/Config.hs
+++ dnsext-bowline/clove/Config.hs
@@ -16,6 +16,7 @@ data Config = Config
     , cnf_tcp_port :: PortNumber
     , cnf_udp_addrs :: [String]
     , cnf_udp_port :: PortNumber
+    , cnf_allow_axfr :: [String]
     }
 
 defaultConfig :: Config
@@ -27,6 +28,7 @@ defaultConfig =
         , cnf_tcp_port = 53
         , cnf_udp_addrs = ["127.0.0.1", "::1"]
         , cnf_udp_port = 53
+        , cnf_allow_axfr = ["127.0.0.1", "::1"]
         }
 
 makeConfig :: Config -> [Conf] -> IO Config
@@ -37,6 +39,7 @@ makeConfig def conf = do
     cnf_tcp_port <- get "tcp-port" cnf_tcp_port
     cnf_udp_addrs <- get "udp-addrs" cnf_udp_addrs
     cnf_udp_port <- get "udp-port" cnf_udp_port
+    cnf_allow_axfr <- get "allow-axfer" cnf_allow_axfr
     pure Config{..}
   where
     get k func = do
