aa8706169da0a8768c6d06758c972eadbc93c0a5
Kei Hibino <ex8k.hibino@gmail.com>
2026-03-23 18:59:48 +0900

    dnsext-types: export utils

--- dnsext-types/DNS/Types.hs
+++ dnsext-types/DNS/Types.hs
@@ -252,6 +252,15 @@ module DNS.Types (
     -- ** Errors
     DNSError (..),
     unwrapDNSErrorInfo,
+    networkFailure,
+    fromIOException,
+    catchDNS',
+    catchDNS,
+    handleDNS',
+    handleDNS,
+    tryDNS',
+    tryDNS,
+    timeoutDNS,
 
     -- ** Seconds
     Seconds (..),
