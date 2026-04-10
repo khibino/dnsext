66698dcd142d5ea81171316f80ee242cc2da5cf5
Kazu Yamamoto <kazu@iij.ad.jp>
2026-04-10 19:50:14 +0900

    simplified arithmetic

--- dnsext-types/DNS/Types/Serial.hs
+++ dnsext-types/DNS/Types/Serial.hs
@@ -2,7 +2,7 @@
 
 module DNS.Types.Serial where
 
-import Data.Word (Word32, Word64)
+import Data.Word (Word32)
 
 newtype Serial = Serial {unSerial :: Word32} deriving (Eq)
 
@@ -12,11 +12,7 @@ instance Show Serial where
 instance Ord Serial where
     Serial s1 <= Serial s2 =
         s1 == s2
-            || (i1 < i2 && i2 - i1 < mx)
-            || (i1 > i2 && i1 - i2 > mx)
+            || (s1 < s2 && (s2 - s1) < mx)
+            || (s1 > s2 && (s1 - s2) > mx)
       where
-        i1 :: Word64
-        i1 = fromIntegral s1
-        i2 :: Word64
-        i2 = fromIntegral s2
         mx = 2 ^ (31 :: Int)
