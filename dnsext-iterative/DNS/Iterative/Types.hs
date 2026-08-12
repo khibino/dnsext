709be1d7ed711a0ebe0b2ac2d7f60d4dfa4d2c53
Kei Hibino <ex8k.hibino@gmail.com>
2026-08-12 20:42:48 +0900

    dnsext-iterative: drop old moved modules

--- dnsext-iterative/DNS/Iterative/Types.hs
+++
@@ -1,5 +0,0 @@
-module DNS.Iterative.Types {-# DEPRECATED "import DNS.Transport.Types instead of this" #-} (
-    module DNS.Transport.Types,
-) where
-
-import DNS.Transport.Types
--- dnsext-iterative/DNS/Iterative/WorkerStats.hs
+++
@@ -1,5 +0,0 @@
-module DNS.Iterative.WorkerStats {-# DEPRECATED "import DNS.WorkerStats instead ot this" #-} (
-    module DNS.WorkerStats,
-) where
-
-import DNS.WorkerStats
