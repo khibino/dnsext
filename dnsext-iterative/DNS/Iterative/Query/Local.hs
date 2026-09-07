f4053622770ad050d5daf3b402c0ef6d49e440a5
Kei Hibino <ex8k.hibino@gmail.com>
2026-09-07 14:14:17 +0900

    dnsext-iterative: local: takeLocals for synthesis

--- dnsext-iterative/DNS/Iterative/Query/Local.hs
+++ dnsext-iterative/DNS/Iterative/Query/Local.hs
@@ -1,13 +1,21 @@
 module DNS.Iterative.Query.Local where
 
--- dnsext packages
+-- GHC packages
+import qualified Data.Map.Strict as Map
+
+-- dnsext-types
 import DNS.Types
+
+-- dnsext-utils
+import DNS.Transport.Types (Synthesis (..))
 
 -- this package
 
 import DNS.Iterative.Query.Class
 import DNS.Iterative.Query.LocalZone (lookupApex, lookupName)
 import DNS.Iterative.Query.StubZone (lookupStub)
+
+type TakeLocal a = Env -> Question -> a -> a -> (ResultRRS -> a) -> a
 
 {- FOURMOLU_DISABLE -}
 takeLocalResult :: Env -> Question -> a -> a -> (ResultRRS -> a) -> a
@@ -17,3 +25,22 @@ takeLocalResult env q@Question{qname=dom,qclass=cls} denied nothing just
     | IN <- cls, (apexes, names) <- localZones_ env, Just apex <- lookupApex apexes dom = maybe denied just $ lookupName names apex q
     | otherwise                                                                         = nothing
 {- FOURMOLU_ENABLE -}
+
+takeLocalSynthNone :: Env -> Question -> a -> a -> (ResultRRS -> a) -> a
+takeLocalSynthNone = takeLocalSynthZone SynthNone
+
+takeLocalSynthDNS64 :: Env -> Question -> a -> a -> (ResultRRS -> a) -> a
+takeLocalSynthDNS64 = takeLocalSynthZone SynthDNS64
+
+{- FOURMOLU_DISABLE -}
+takeLocalSynthZone :: Synthesis -> Env -> Question -> a -> a -> (ResultRRS -> a) -> a
+takeLocalSynthZone synth env q@Question{qname=dom,qclass=cls} denied nothing just
+    | IN <- cls, Just{} <- lookupStub (stubZones_ env) dom                              = nothing  {- stubs supersede locals -}
+    | IN <- cls, Just (apexes, names) <- synthZones, Just apex <- lookupApex apexes dom = maybe denied just $ lookupName names apex q
+    | otherwise                                                                         = nothing
+  where
+    synthZones = Map.lookup synth (localSynthZones_ env)
+{- FOURMOLU_ENABLE -}
+
+takeLocalNoop :: Env -> Question -> a -> a -> (ResultRRS -> a) -> a
+takeLocalNoop _env _q _denied nothing _just = nothing
